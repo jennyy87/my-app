57b8363c4ffa4638d322bcca1e592a305e25218a
Jennifer Yi <jenniferyi898@gmail.com>
2020-05-30 19:43:39 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>George Sandle.</h1>
+
+
+    </div>
+  );
+}
+
+export default App;
